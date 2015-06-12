49acff4eb5d2f9376694e18c9fe06272767126b4
The Mini John <the@minijohn.me>
2015-06-13 01:15:10 +0300

    Add Friendly Id to Airlines

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,7 +1,14 @@
 class Airline < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :airline_slug
+
   has_one :trip
 
   def select_name
   	"(#{iata}) #{name}"
+  end
+
+  def airline_slug
+    "#{iata} #{name}"
   end
 end
--- db/migrate/20150610080932_create_airlines.rb
+++ db/migrate/20150610080932_create_airlines.rb
@@ -7,11 +7,13 @@ class CreateAirlines < ActiveRecord::Migration
       t.string :name
       t.string :phone_number
       t.boolean :active
+      t.string :slug
 
       t.timestamps null: false
     end
     add_index :airlines, :icao
     add_index :airlines, :name
     add_index :airlines, :active
+    add_index :airlines, :slug, unique: true
   end
 end
